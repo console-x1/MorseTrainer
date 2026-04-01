1659fdfda00965a436dcce1e9019c6ce29497e51
console-x1 <x1.fonda@gmail.com>
2026-04-01 17:25:26 -0300

    Update power of letters

--- morse.js
+++ morse.js
@@ -6,12 +6,12 @@ const level5 = [...level4, "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 const level6 = [...level5, ".", ",", "?", "'", "!", "/"];
 
 const LEVELS = {
-  1: level1,
-  2: level2,
-  3: level3,
-  4: level4,
-  5: level5,
-  6: level6
+    1: level1,
+    2: level2,
+    3: level3,
+    4: level4,
+    5: level5,
+    6: level6
 };
 
 const MORSE = {
@@ -59,18 +59,63 @@ const MORSE = {
     "/": "-..-."
 }
 
+const FREQUENCIES = {
+    E: 8, 
+    T: 8, 
+    A: 8, 
+    I: 8, 
+    N: 8, 
+    S: 7,
+    R: 7, 
+    O: 7, 
+    L: 5, 
+    U: 5,
+    M: 5, 
+    D: 5, 
+    G: 3,
+    B: 4, 
+    C: 4, 
+    P: 3,
+    F: 3, 
+    H: 3, 
+    J: 3, 
+    K: 1, 
+    Q: 1,
+    V: 1, 
+    W: 1, 
+    X: 1, 
+    Y: 1,
+    Z: 1,
+    
+    '1': 2, '2': 2, '3': 2, '4': 2, '5': 2, '6': 2, '7': 2, '8': 2, '9': 2, '0': 2,
+    '.': 3, ',': 3, '?': 2, "'": 1, '!': 3, '/': 1
+};
+
 function getRandomLetter(level = 4, lastletter) {
-    let letter = '';
-    if (level == 1) letter = level1[Math.floor(Math.random() * level1.length)];
-    else if (level == 2) letter = level2[Math.floor(Math.random() * level2.length)];
-    else if (level == 3) letter = level3[Math.floor(Math.random() * level3.length)];
-    else if (level == 4) letter = level4[Math.floor(Math.random() * level4.length)];
-    else if (level == 5) letter = level5[Math.floor(Math.random() * level5.length)];
-    else letter = level6[Math.floor(Math.random() * level6.length)];
+    const allowed = LEVELS[level];
+    const previous = LEVELS[level - 1] || [];
 
-    if (letter == lastletter && level !== 1) return getRandomLetter(level, lastletter)
+    let pool = [];
 
-    return letter
+    for (const char of allowed) {
+        let weight = FREQUENCIES[char] || 1;
+
+        if (!previous.includes(char)) {
+            weight *= 3;
+        }
+
+        for (let i = 0; i < weight; i++) {
+            pool.push(char);
+        }
+    }
+
+    while (true) {
+        const char = pool[Math.floor(Math.random() * pool.length)];
+
+        if (char !== lastletter) {
+            return char;
+        }
+    }
 }
 
 module.exports = { LEVELS, MORSE, getRandomLetter };
